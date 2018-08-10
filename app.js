475c58435675c35a6b1b835531e1cd6fef3c92a9
Michael Ilyash <mihailo_rlyash@mail.ru>
2018-08-10 09:00:07 +0300

    Update app.js

--- app.js
+++ app.js
@@ -56,6 +56,24 @@ MongoClient.connect(MONGO_URL, function(err, db){
 		});
     });
 //-----кінець видалення	
+	app.put('/userlistsocet', function (req, res) {
+		var fname1 = req.query.fname;
+				
+		db.collection("userlistsocet").find({user_name: fname1}).toArray(function(error, doc) {
+		    if (err) throw error;
+			console.log('doc',doc);			
+			 db.collection("userlistsocet").findAndModify({
+				query: {_id:doc._id},
+				update: {$set: {user_paytime: req.body.user_paytime}},//change avtomatic change
+				new: true}, function (err, doc) {
+					res.json(doc);
+				}
+			);
+		});
+			
+    });	
+	
+	
 	
    io.on('connection', (socket) => {
 		//io.emit('fetch rooms', rooms);
