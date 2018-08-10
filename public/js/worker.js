21600afc8aa609b397e822961ad734d6faf4e5c8
Michael Ilyash <mihailo_rlyash@mail.ru>
2018-08-10 08:04:03 +0300

    Update worker.js

--- public/js/worker.js
+++ public/js/worker.js
@@ -252,48 +252,7 @@ $(document).on('click', '.userp', (ev) => {
 			console.log('task',$('#task').val());
 			time = $('#time').val();
 			task = $('#task').val();
-			
-			var avatar;
-				if ($('#picrscr').val()==''){
-					avatar='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXPg-87YPJhgdeqQoAlUdgF60k6yi61LlpDtSXSqjWMVa9xbWVXQ';
-				}else{
-					avatar=$('#picrscr').val();
-				}
-				console.log('task111',task,time);
-				if ((task!==null)&&(task>'')){
-					var logs = task+'Time:'+time;
-					console.log('task',logs);
-					socket.emit('chat message', {
-						room_id: current_user_fix,
-						user_avatar: avatar,		
-						user_name: user_name,
-						message: logs
-					});
-				}
-			if (current_user_fix==$('#response').text()){
-
-				socket.emit('join', {
-					room_id: current_user_fix,
-					user_name: user_name
-				});
-
-				$.ajax({
-					url: "/reactlist",
-					type: "GET",
-					data: '',
-					cache: false,
-					success: function(response){
-						console.log('success user');				
-					}
-				});								
-				socket.emit('fetch message', current_user_fix);
-				$('#current_room_id').text(current_user_fix);		
-			}
-
-			
-			document.getElementById('id01').style.display='none';	
-		
-/*		
+					
 		var avatar;
 		if ($('#picrscr').val()==''){
 			avatar='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXPg-87YPJhgdeqQoAlUdgF60k6yi61LlpDtSXSqjWMVa9xbWVXQ';
@@ -331,7 +290,6 @@ $(document).on('click', '.userp', (ev) => {
 		socket.emit('fetch message', current_user_fix);
 		$('#current_room_id').text(current_user_fix);		
 	}
-*/	
 	return false;
 });
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
